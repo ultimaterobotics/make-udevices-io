73c092ccdb204f2ec242e97a2268ad82c0b2ac07
sohryu-l <cross.triforce@gmail.com>
2024-08-13 20:23:21 +0300

    a lot of guides added, old pages moved from hackaday

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,19 +7,48 @@ export default defineConfig({
 		starlight({
 			title: 'Start learning!',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/ultimaterobotics',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'uDevices',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Getting started with uDevices', link: '/guides/example/' },
+						{ label: 'Getting started with uDevices', link: '/guides/getting-started-with-udevices/' },
+						{ label: 'Quick start - uMyo', link: '/guides/umyo-quick-start/'},
+						{ label: 'Quick start - uECG', link: '/guides/uecg-quick-start'},
+
+					]
+				}
+			,
+				{
+					label: 'uMyo',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'uMyo - wearable EMG sensor', link: '/guides/umyo/' },
+						{ label: 'Description', link: '/guides/umyo-description-hackaday/' },
+						{ label: 'Getting started with uMyo', link: '/guides/umyo-getting-started-hackaday'},
+						{ label: 'uMyo - Power On/Off', link: '/guides/umyo-power-on-off-hackaday'},
+						{ label: 'uMyo - Using uMyo with bracelet and dry connectors', link: '/guides/umyo-using-bracelet-hackaday'},
+						{ label: 'uMyo - Using uMyo with wet electrodes', link: '/guides/umyo-wet-electrodes-hackaday'},
+						{ label: 'uMyo - Using uMyo with Arduino/ESP32', link: '/guides/umyo-arduino-hackaday'},
+						{ label: 'uMyo - Using 2 uMyo devices', link: '/guides/umyo-2-umyos-hackaday'},
+						{ label: 'Demo projects', link: '/guides/umyo-demo-projects'},
+
 					],
 				},
 				{
-					label: 'References',
-					autogenerate: { directory: 'reference' },
+					label: 'uECG',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'uECG - wearable ECG sensor', link: '/guides/uecg/'},
+						{ label: 'Description', link: '/guides/uecg-description-hackaday/'},
+						{ label: 'uECG - Basic Guide', link: '/guides/uecg-basic-hackaday/'},
+						{ label: 'uECG - Using the App', link: '/guides/uecg-the-app-hackaday/'},
+						{ label: 'uECG - Desktop App', link: '/guides/uecg-desktop-app-hackaday/'},
+						{ label: 'uECG - Wired Firmware Update', link: '/guides/uecg-wired-firmware-hackaday/'},
+						{ label: 'uECG - Wireless Firmware Update', link: '/guides/uecg-wireless-firmware-hackaday/'},
+					]
 				},
 			],
 		}),
