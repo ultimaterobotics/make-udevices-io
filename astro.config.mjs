93cc83c506bf20e4e3ce4d7cd758c16c337f4975
astartav <astarta.v@gmail.com>
2025-05-31 21:56:32 +0300

    Updated Astro/Starlight, added shop link, anonymized GA tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,59 +3,89 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'uDevices',
-			logo: {
-				light: './src/assets/uDevices_logo_short_light.svg',
-				dark: './src/assets/uDevices_logo_short_dark.svg',
-			  },
-			social: {
-				github: 'https://github.com/ultimaterobotics',
-			},
-			sidebar: [
-				{
-					label: 'uDevices',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Getting started with uDevices', link: '/guides/getting-started-with-udevices/' },
-						{ label: 'Quick start - uMyo', link: '/guides/umyo-quick-start/'},
-						{ label: 'Quick start - uECG', link: '/guides/uecg-quick-start'},
+    integrations: [
+        starlight({
+            title: 'uDevices',
+            head: [
+                {
+                    tag: 'script',
+                    attrs: {
+                        src: 'https://www.googletagmanager.com/gtag/js?id=G-BKQX2Y6S76',
+                        async: true,
+                        is: 'inline', 
+                    },
+                },
+                {
+                    tag: 'script',
+                    attrs: {
+                        is: 'inline', // Inline the script
+                    },
+                    children: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BKQX2Y6S76', { anonymize_ip: true });
+          `,
+                },
+            ],
+            logo: {
+                light: './src/assets/uDevices_logo_short_light.svg',
+                dark: './src/assets/uDevices_logo_short_dark.svg',
+            },
+            social: [
+                { icon: 'discord', label: 'Discord', href: 'https://discord.gg/9cbBHxRt' },
+                {
+                    icon: 'github',
+                    label: 'GitHub',
+                    href: 'https://github.com/ultimaterobotics',
+                },
+            ],
+            sidebar: [
+                {
+                    label: 'uDevices',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Getting started with uDevices', link: '/guides/getting-started-with-udevices/' },
+                        { label: 'Quick start - uMyo', link: '/guides/umyo-quick-start/' },
+                        { label: 'Quick start - uECG', link: '/guides/uecg-quick-start' },
 
-					]
-				}
-			,
-				{
-					label: 'uMyo',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'uMyo - wearable EMG sensor', link: '/guides/umyo/' },
-						{ label: 'Description', link: '/guides/umyo-description-hackaday/' },
-						{ label: 'Getting started with uMyo', link: '/guides/umyo-getting-started-hackaday'},
-						{ label: 'uMyo - Power On/Off', link: '/guides/umyo-power-on-off-hackaday'},
-						{ label: 'uMyo - Using uMyo with bracelet and dry connectors', link: '/guides/umyo-using-bracelet-hackaday'},
-						{ label: 'uMyo - Using uMyo with wet electrodes', link: '/guides/umyo-wet-electrodes-hackaday'},
-						{ label: 'uMyo - Using uMyo with Arduino/ESP32', link: '/guides/umyo-arduino-hackaday'},
-						{ label: 'uMyo - Using 2 uMyo devices', link: '/guides/umyo-2-umyos-hackaday'},
-						{ label: 'How to upload uMyo firmware from Windows', link: '/guides/how-to-upload-firmware-on-umyo-windows'},
-						{ label: 'Demo projects', link: '/guides/umyo-demo-projects'},
+                    ]
+                }
+                ,
+                {
+                    label: 'uMyo',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'uMyo - wearable EMG sensor', link: '/guides/umyo/' },
+                        { label: 'Description', link: '/guides/umyo-description-hackaday/' },
+                        { label: 'Getting started with uMyo', link: '/guides/umyo-getting-started-hackaday' },
+                        { label: 'uMyo - Power On/Off', link: '/guides/umyo-power-on-off-hackaday' },
+                        { label: 'uMyo - Using uMyo with bracelet and dry connectors', link: '/guides/umyo-using-bracelet-hackaday' },
+                        { label: 'uMyo - Using uMyo with wet electrodes', link: '/guides/umyo-wet-electrodes-hackaday' },
+                        { label: 'uMyo - Using uMyo with Arduino/ESP32', link: '/guides/umyo-arduino-hackaday' },
+                        { label: 'uMyo - Using 2 uMyo devices', link: '/guides/umyo-2-umyos-hackaday' },
+                        { label: 'How to upload uMyo firmware from Windows', link: '/guides/how-to-upload-firmware-on-umyo-windows' },
+                        { label: 'Demo projects', link: '/guides/umyo-demo-projects' },
 
-					],
-				},
-				{
-					label: 'uECG',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'uECG - wearable ECG sensor', link: '/guides/uecg/'},
-						{ label: 'Description', link: '/guides/uecg-description-hackaday/'},
-						{ label: 'uECG - Basic Guide', link: '/guides/uecg-basic-hackaday/'},
-						{ label: 'uECG - Using the App', link: '/guides/uecg-the-app-hackaday/'},
-						{ label: 'uECG - Desktop App', link: '/guides/uecg-desktop-app-hackaday/'},
-						{ label: 'uECG - Wired Firmware Update', link: '/guides/uecg-wired-firmware-hackaday/'},
-						{ label: 'uECG - Wireless Firmware Update', link: '/guides/uecg-wireless-firmware-hackaday/'},
-					]
-				},
-			],
-		}),
-	],
+                    ],
+                },
+                {
+                    label: 'uECG',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'uECG - wearable ECG sensor', link: '/guides/uecg/' },
+                        { label: 'Description', link: '/guides/uecg-description-hackaday/' },
+                        { label: 'uECG - Basic Guide', link: '/guides/uecg-basic-hackaday/' },
+                        { label: 'uECG - Using the App', link: '/guides/uecg-the-app-hackaday/' },
+                        { label: 'uECG - Desktop App', link: '/guides/uecg-desktop-app-hackaday/' },
+                        { label: 'uECG - Wired Firmware Update', link: '/guides/uecg-wired-firmware-hackaday/' },
+                        { label: 'uECG - Wireless Firmware Update', link: '/guides/uecg-wireless-firmware-hackaday/' },
+                    ]
+                },
+            ],
+            editLink: {
+                baseUrl: 'https://github.com/ultimaterobotics/make-udevices-io/edit/main/',
+            },
+        }),
+    ],
 });
